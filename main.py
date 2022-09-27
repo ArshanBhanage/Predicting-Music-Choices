b5a220329be7144ad30c99dd99a9830115d4818e
Arshan Bhanage <68952822+ArshanBhanage@users.noreply.github.com>
2022-09-27 14:53:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import warnings
+
+from sklearn import metrics
+
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv("data.csv")
+data = data[0:5000]
+
+genre_data = pd.read_csv('data_by_genres.csv')
+
+print(data.info())
+
+top10_genres = genre_data.nlargest(10, 'popularity')
+
+fig = px.bar(top10_genres, x='genres', y=['valence', 'energy', 'danceability', 'acousticness'], barmode='group')
+fig.show()
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+cluster_pipeline = Pipeline([('scaler', StandardScaler()), ('kmeans', KMeans(n_clusters=10))])
+X = genre_data.select_dtypes(np.number)
+cluster_pipeline.fit(X)
+genre_data['cluster'] = cluster_pipeline.predict(X)
+
+# Visualizing the Clusters with t-SNE
+
+from sklearn.manifold import TSNE
+
+tsne_pipeline = Pipeline([('scaler', StandardScaler()), ('tsne', TSNE(n_components=2, verbose=1))])
+genre_embedding = tsne_pipeline.fit_transform(X)
+projection = pd.DataFrame(columns=['x', 'y'], data=genre_embedding)
+projection['genres'] = genre_data['genres']
+projection['cluster'] = genre_data['cluster']
+
+fig = px.scatter(
+    projection, x='x', y='y', color='cluster', hover_data=['x', 'y', 'genres'])
+fig.show()
+
+song_cluster_pipeline = Pipeline([('scaler', StandardScaler()),
+                                  ('kmeans', KMeans(n_clusters=20,
+                                                    verbose=False))
+                                  ], verbose=False)
+
+X = data.select_dtypes(np.number)
+number_cols = list(X.columns)
+song_cluster_pipeline.fit(X)
+song_cluster_labels = song_cluster_pipeline.predict(X)
+data['cluster_label'] = song_cluster_labels
+
+# Visualizing the Clusters with PCA
+
+from sklearn.decomposition import PCA
+
+pca_pipeline = Pipeline([('scaler', StandardScaler()), ('PCA', PCA(n_components=2))])
+song_embedding = pca_pipeline.fit_transform(X)
+projection = pd.DataFrame(columns=['x', 'y'], data=song_embedding)
+projection['title'] = data['name']
+projection['cluster'] = data['cluster_label']
+
+fig = px.scatter(
+    projection, x='x', y='y', color='cluster', hover_data=['x', 'y', 'title'])
+fig.show()
